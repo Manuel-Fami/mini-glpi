9fe006495885e20e68658a92e4882f080d1b9ee7
Manuel <manuelfamiliar@laposte.net>
2026-02-28 14:59:35 +0100

    Page connexion + accueil et installation tailwind

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -7,7 +7,7 @@ use App\Repository\TicketRepository;
 use App\Service\TicketService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/tickets')]
 class TicketController extends AbstractController
